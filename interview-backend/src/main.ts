d95b32143da3a4b21f0192ffd377bea421cc08b3
Nikki Rostollan <nrostollan@gmail.com>
2019-12-21 13:11:45 -0600

    Update & log api port

--- interview-backend/src/main.ts
+++ interview-backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from "@nestjs/core";
+import { Logger } from "@nestjs/common";
 import { AppModule } from "./app.module";
 import {ConfigService} from "./config/config.service";
 
@@ -6,7 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const config = app.get(ConfigService);
-  await app.listen(config.get("PORT") || 6000);
+  const logger = app.get(Logger);
+  const port = config.get("PORT") || 5000;
+  logger.log(port, "ApplicationPort");
+  await app.listen(port);
 }
 
 bootstrap();
