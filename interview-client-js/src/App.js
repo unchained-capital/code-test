a3b5bb07520b2cc28f76562626ff9972b3d15dc5
Justin Moore <humanumbrella@gmail.com>
2020-10-30 14:24:00 -0700

    bump deps and edit starting screen

--- interview-client-js/src/App.js
+++ interview-client-js/src/App.js
@@ -6,12 +6,11 @@ async function getTodos() {
 
 const App = () => {
   useEffect(() => {
-    // @ts-ignore
-    getTodos().then(console.log);
+    getTodos().then(console.log("Welcome."));
   }, []);
 
 
-  return null;
+  return (<div style={{marginTop: "2em", textAlign: "center"}}>It looks like you have everything set up properly, good luck.</div>);
 };
 
 export default App;
